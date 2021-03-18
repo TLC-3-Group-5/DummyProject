ef0f91559f0946256debde39309c27bc028381e9
Smart <kwesi.smart@turntabl.io>
2021-03-18 21:37:39 +0000

    init

---
+++ src/test/java/com/example/demo/dummyproject/DummyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.dummyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
